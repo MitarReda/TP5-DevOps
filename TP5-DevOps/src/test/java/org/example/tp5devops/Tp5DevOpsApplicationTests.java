476e4c24c09bc787e42d209dcbad178121bb4d3d
MitarReda <rmitar@OMNISHORE333.onmicrosoft.com>
2025-01-25 15:06:12 +0100

    TP5 devops

---
+++ TP5-DevOps/src/test/java/org/example/tp5devops/Tp5DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp5devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
